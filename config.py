fe4a1c0b093b1d650a30a2329ff22aac8d9e5dae
mihailpy02 <mihailpy02@gmail.com>
2020-12-28 12:37:12 +0300

    Start git

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
-admin_id = "1339488902"
-admins_id = ["1339488902"]
+admin_id = ""
 
-TOKEN = "1439161353:AAGBsGqvNIIb26Kgye-gTIfm6OnHVq9sQmA"
-HOST = "172.17.0.3"
-PORT = "3306"
-USER = "root"
-PASSWORD = "rootpass"
+TOKEN = ""
+HOST = ""
+PORT = ""
+USER = ""
+PASSWORD = ""
