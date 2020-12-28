b75d873a51dc4076a685a1c201d94097bd907982
mihailpy02 <mihailpy02@gmail.com>
2020-12-28 12:37:12 +0300

    add commit

--- createdb.sql
+++ createdb.sql
@@ -1,4 +1,12 @@
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
+SET NAMES utf8mb4;
+
+DROP DATABASE IF EXISTS `adminChat`;
+CREATE DATABASE `adminChat` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin */;
 USE `adminChat`;
 
 DROP TABLE IF EXISTS `users`;
